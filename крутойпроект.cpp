e7b2c1a35076d63f3d7724d73aaabc131184df83
Watson2131 <evgeniysologub090@gmail.com>
2026-09-21 20:55:16 +0300

    добавил регистрацию

--- крутойпроект.cpp
+++ крутойпроект.cpp
@@ -23,29 +23,29 @@ void admin() {
 
 }
 
-void registration(vector<User>& users, int& nextId) {
+bool registration(vector<User>& users, int& nextId) {
     string name;
     int age;
     string pass;
-    
-    while(true) {
+
+    while (true) {
         cout << "===== РЕГИСТРАЦИЯ =====\n";
         cout << "== Для выхода введите Y ==\n";
         cout << "== на первых двух этапах ==\n\n";
 
         cout << "Придумайте имя: ";
         cin >> name;
-        if(name == "Y" || name == "y") {
+        if (name == "Y" || name == "y") {
             break;
         }
         cout << "Придумайте пароль: ";
         cin >> pass;
-        if(pass == "Y" || pass == "y") {
+        if (pass == "Y" || pass == "y") {
             break;
         }
         cout << "Введите возраст: ";
         cin >> age;
-        
+
         if (cin.fail()) {
             cout << "Возраст введен неверно. Повторите попытку!\n";
             cin.clear();
@@ -54,10 +54,9 @@ void registration(vector<User>& users, int& nextId) {
         }
 
         nextId++;
-
         cout << "Регистрация успешна!\n";
         users.push_back(User(nextId, name, age, pass));
-        break;
+        return true;
     }
 }
 
@@ -75,29 +74,31 @@ void menu(vector<User>& users, int& nextId) {
     cin >> choice;
 
     // ---Система меню авторизации---
-    switch(choice) {
-        case 1:
-            registration(users, nextId);
-            break;
-        case 2:
-            break;
-        case 3:
-           char y_n;
-           cout << "Вы уверены?(y/n): ";
-           cin >> y_n;
-           
-           if(y_n == 'y') {
-                cout << "Пока!";
-                exit(0);
-           } else if (y_n == 'n') {
-                break;
-           } else {
-                cout << "Неверный ввод!\n\n";
-                break;
-           }
-    // ---------------------------
+    switch (choice) {
+    case 1:
+        registration(users, nextId);
+        break;
+    case 2:
+        break;
+    case 3:
+        char y_n;
+        cout << "Вы уверены?(y/n): ";
+        cin >> y_n;
 
-    // ------------------
+        if (y_n == 'y') {
+            cout << "Пока!";
+            exit(0);
+        }
+        else if (y_n == 'n') {
+            break;
+        }
+        else {
+            cout << "Неверный ввод!\n\n";
+            break;
+        }
+        // ---------------------------
+
+        // ------------------
     }
 }
 
@@ -108,9 +109,13 @@ void menu2() {
 int main() {
     vector<User> users;
     int nextId = 0;
-    
-    while(true) {
+
+    while (true) {
+        setlocale(LC_ALL, "Russian");
         menu(users, nextId);
+        if (menu(users, nextId)) {
+            break;
+        }
     }
     return 0;
 }
